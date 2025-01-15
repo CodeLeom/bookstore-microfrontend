494acf047e2767cb566dc308e90b9476cb533056
Aransiola Ayodele <36636824+CodeLeom@users.noreply.github.com>
2025-01-15 16:38:48 +0100

    feat: add container code files

---
+++ container/webpack.config.js
@@ -0,0 +1,56 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { use } = require('react');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  devServer: {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+    port: 3000,
+    historyApiFallback: true
+  },
+  output: {
+    publicPath: 'auto',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react"
+            ]
+          }
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader", "postcss-loader"]
+      }
+    ],
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'container',
+      remotes: {
+        listing: "listing@http://localhost:3001/remoteEntry.js",
+        cart: "cart@http://localhost:3002/remoteEntry.js",
+        checkout: "checkout@http://localhost:3003/remoteEntry.js"
+      },
+      shared: ['react', 'react-dom'],
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+};
